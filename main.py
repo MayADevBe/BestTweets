e5e9b2bb35a50d3d73ec6bf5638e94f3bfa13320
MayADevBe <o3wlcodes.gmail.com>
2020-03-13 14:34:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ def get_most_liked(user, time):
     best_tweets = {}
     for tweet in api.user_timeline(screen_name=user, count=3):
         try:
+            #check if retweet
             retweet = tweet.retweeted_status
             print("Tweet is a retweet")
         except:
-            if tweet.created_at > time and tweet.retweeted == False:
+            # check if tweet in timeframe
+            if tweet.created_at > time:
+                #count all interaction
                 sum = tweet.retweet_count + tweet.favorite_count 
                 try:
                     sum += tweet.reply_count
@@ -22,8 +25,18 @@ def get_most_liked(user, time):
                     sum += tweet.quote_count
                 except:
                     print("No Quotes")
-                best_tweets[tweet.id] = sum
-                print(tweet.id, tweet.text, sum)
+                try:
+                    url = tweet.entities['media'][0]['expanded_url']
+                except:
+                    try:
+                        url = tweet.entities['urls'][0]['expanded_url']
+                    except:
+                        print("Couldn't get url")
+                        break
+                best_tweets[url] = sum
+            else: 
+                break
     print(best_tweets)
+
 time = datetime.now() - timedelta(days=365)
 get_most_liked('MayADevBe', time)
