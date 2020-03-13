c66122af335767e71979189f59bed2eaa6bda1a9
MayADevBe <o3wlcodes.gmail.com>
2020-03-13 14:04:49 +0100

    get tweets + count interaction + no retweets

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ ACCESS_TOKEN_SECRET = ''
 
 def get_Api():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    #auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     return api
--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import tweepy
-import time
+from datetime import datetime
+from datetime import timedelta
 import config
 
 api = config.get_Api()
 
-def get_most_liked(time, user):
-    for tweet in api.user_timeline(screen_name=user).items(1):
-        print(tweet)
+def get_most_liked(user, time):
+    best_tweets = {}
+    for tweet in api.user_timeline(screen_name=user, count=3):
+        try:
+            retweet = tweet.retweeted_status
+            print("Tweet is a retweet")
+        except:
+            if tweet.created_at > time and tweet.retweeted == False:
+                sum = tweet.retweet_count + tweet.favorite_count 
+                try:
+                    sum += tweet.reply_count
+                except:
+                    print("No Replys")
+                try:
+                    sum += tweet.quote_count
+                except:
+                    print("No Quotes")
+                best_tweets[tweet.id] = sum
+                print(tweet.id, tweet.text, sum)
+    print(best_tweets)
+time = datetime.now() - timedelta(days=365)
+get_most_liked('MayADevBe', time)
